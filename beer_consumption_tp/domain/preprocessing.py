55219c34737835dfd68472e0a764beacd2c89cec
ZROUAMA FRANCK HERMANN DEBAH <debah2002@gmail.com>
2022-12-14 12:19:42 +0000

    ajout de la fonction find_max

--- beer_consumption_tp/domain/preprocessing.py
+++ beer_consumption_tp/domain/preprocessing.py
@@ -1,7 +1,8 @@
 # fonction qui trouve la brasserie donnant le plus gros ABV
 def find_max_features_by_features(df,feature_to_group,feature_to_find_max):
-    # grouper par brasserie
-    df = df.groupby(feature_to_group)
-    # trouver la brasserie avec le plus gros ABV
-    max_abv_brewery = df[feature_to_find_max].max().idxmax()
-    return max_abv_brewery
+    return df.groupby(feature_to_group)[feature_to_find_max].max().idxmax()
+    
+# fonction qui transforme le timestamp en date
+def transform_timestamp_to_date(df,pd):
+    df['date'] = pd.to_datetime(df['review_time'],unit='s')
+    return df
